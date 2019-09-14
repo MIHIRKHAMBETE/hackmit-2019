ef3c1b3e3d232bc63890fb0843418d42842cd4b1
Mihir <mkhambet@mit.edu>
2019-09-14 19:46:04 -0400

    Added basic functionality for hello world flask app

--- extract_car_data.py
+++ extract_car_data.py
@@ -6,6 +6,7 @@ Created on Sat Sep 14 16:36:48 2019
 @author: mihirkhambete
 """
 import json
+#import requests
 #trying to push
 """
 Parses car trace json file to get fuel, latitude, longitude, odometer
@@ -19,20 +20,32 @@ def get_car_data():
     with open('downtown-crosstown.json') as f:
         for line in f:
             data = json.loads(line)
-            
+            post_data = {}
             if data["name"] == "fuel_level":
                 current_fuel = data["value"]
                 fuel_data.append(current_fuel)
+                post_data["fuel_level"] = current_fuel
+                
+                
             elif data["name"] == "latitude":
                 current_latitude = data["value"]
                 latitude_data.append(current_latitude)
+                post_data_type = "latitude"
+                post_data["curren"] = current_latitude
+                
             elif data["name"] == "longitude":
                 current_longitude = data["value"]
                 longitude_data.append(current_longitude)
+                post_data_type = "longitude"
+                post_data_value = current_longitude
             elif data["name"] == "odometer":
                 current_odometer = data["value"]
                 odometer_data.append(current_odometer)
+                post_data_type = "odometer"
+                post_data_value = current_odometer
             else:
                 continue
+            
+            #POST to suzie's URL
     return (fuel_data, latitude_data, longitude_data, odometer_data)
 
---
+++ patient_information.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 15:28:48 2019
+
+@author: mihirkhambete
+"""
+"""
+ store patient data as a 'Database' object
+ should have Red, Yellow, Green categories
+ each patient (regardless of category) has the following info:
+     patient ID# (int)
+     GPS Location (coordinates - use geopy or google maps for this?)
+     Location details (string)
+     Time Triaged (can use Unix Standard Time?)
+     Conditions (list of strings)
+     Status (found by first responders, ambulance called, picked up)
+"""
+
+"""
+First Responders:
+    add a new patient to the patient database
+"""
+
+"""
+Incident Commander:
+    view all patients
+"""
+
+"""
+Dispatch - view ambulance info
+"""
