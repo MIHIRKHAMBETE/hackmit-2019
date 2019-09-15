b11eb697f531dbba32cfd9608ad5224c0ba14aa9
AZ <alvinzhu33@yahoo.com>
2019-09-14 21:56:19 -0400

    Updates to functions

--- mci.py
+++ mci.py
@@ -3,7 +3,8 @@ import random
 
 class Patient:
 
-    def __init__(self, location, status, condition, need):
+    def __init__(self, pid, location, status, condition, need):
+        self.id = pid
         self.location = location
         self.status = status
         self.condition = condition
@@ -20,7 +21,8 @@ class Patient:
 
 class Dispatcher:
 
-    def __init__(self):
+    def __init__(self, did):
+        self.id = did
         self.assignee = None
 
     def reassign(self, patientID):
@@ -39,21 +41,38 @@ class MCI:
         self.dispatchers = dispatchers
         self.incidence = True
 
-    def assignPatient(self, dispatcherID, patientID):
-        self.dispatchers[dispatcherID].reassign(patientID)
-        self.patientDict[patientID].reassign(dispatcherID)
-    
     def addPatient(self, location, triage, condition, need):
         pid = random.randint(1000, 9999)
         while pid in self.patientDict:
             pid = random.randint(1000, 9999)
-        patient = Patient(location, triage, condition, need)
+        patient = Patient(pid, location, triage, condition, need)
         self.patientDict[pid] = patient
+
+    def assignPatient(self, dispatcherID):
+        r = None
+        y = None
+        g = None
+        for patientID in self.patientDict:
+            if self.patientDict[patientID].status == 'R' and not r:
+                r = patientID
+            if self.patientDict[patientID].status == 'Y' and not y:
+                y = patientID
+            if self.patientDict[patientID].status == 'G' and not g:
+                g = patientID
+        if r:
+            patientID = r
+        elif y:
+            patientID = y
+        else:
+            patientID = g
+        self.dispatchers[dispatcherID].reassign(patientID)
+        self.patientDict[patientID].reassign(dispatcherID)
 
     def changePatientStatus(self, dispatcherID, patientID, status):
         self.patientDict[patientID].changeStatus(status)
         if status == 'Refused' or status == 'Transported':
             self.patientDone.append(self.patientDict[patientID])
             del self.patientDict[patientID]
+            self.assignPatient(dispatcherID)
         if len(self.patientDict) == 0:
             self.incidence = False
